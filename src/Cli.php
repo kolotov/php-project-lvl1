7bd1f097a78c68cffcead6c120b7a93d740ec750
gkdev <gkolotov.adv@gmail.com>
2023-08-26 16:13:51 +0300

    build: implement greeting and prompt for game

---
+++ src/Cli.php
@@ -0,0 +1,8 @@
+<?php
+namespace BrainGames\Cli;
+use function cli\line;
+use function cli\prompt;
+
+line('Welcome to the Brain Game!');
+$name = prompt('May I have your name?');
+line("Hello, %s!", $name);
