debf1ffb07ed450602b28aa6d366f62a115a3363
gkdev <gkolotov.adv@gmail.com>
2023-08-26 23:34:29 +0300

    refactor: isolate greeting to function

--- src/Cli.php
+++ src/Cli.php
@@ -5,6 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+function greeting(): void
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
